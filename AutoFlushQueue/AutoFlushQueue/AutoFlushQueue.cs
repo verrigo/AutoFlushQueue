774e1159c590a3f93b14374cb7f7ee54bb9feae7
verrigo <radu.caprescu@hotmail.com>
2016-08-20 21:25:13 +0300

    Initial Commit

---
+++ AutoFlushQueue/AutoFlushQueue/AutoFlushQueue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace AutoFlushQueue
+{
+    public class AutoFlushQueue
+    {
+        private System.Timers.Timer _timer;
+        private bool _isFull = false;
+        private int _autoFlushIntervalInSeconds;
+        private int _autoFlushMaximumSize;
+        public Queue<int> Queue { get; set; }
+        private Queue<int> _bufferQueue;
+        public Action<List<int>> OnQueueIsFull { get; set; }
+
+        public bool IsFull
+        {
+            get
+            {
+                return _isFull;
+            }
+            private set { PerformActionOnQueueFull(value); }
+        }
+        private void PerformActionOnQueueFull(bool value)
+        {
+            if (value)
+            {
+                var _allValuesFromQueue = Queue.ToList<int>();
+                Queue.Clear();
+                while (_bufferQueue.Count > 0)
+                {
+                    Enqueue(_bufferQueue.Dequeue());
+                }
+                OnQueueIsFull(_allValuesFromQueue);
+            }
+        }
+
+        public AutoFlushQueue(int autoFlushMaximumSize, int autoFlushInterval)
+        {
+            _autoFlushMaximumSize = autoFlushMaximumSize;
+            Queue = new Queue<int>();
+            _bufferQueue = new Queue<int>();
+            IsFull = false;
+            _autoFlushIntervalInSeconds = autoFlushInterval;
+            SetUpTimer();
+        }
+
+        private void SetUpTimer()
+        {
+            _timer = new System.Timers.Timer(_autoFlushIntervalInSeconds*1000);
+            _timer.AutoReset = true;
+            _timer.Elapsed += HandleTimer;
+            _timer.Enabled = true;
+        }
+
+        private void HandleTimer(Object source, ElapsedEventArgs e)
+        {
+            IsFull = true;
+            Console.WriteLine("In timer");
+        }
+
+        public void Enqueue(int number)
+        {
+            if (IsFull)
+                _bufferQueue.Enqueue(number);
+            else
+            {
+                Queue.Enqueue(number);
+                if (Queue.Count == _autoFlushMaximumSize)
+                {
+                    IsFull = true;
+                }
+            }
+        }
+    }
+}
---
+++ AutoFlushQueue/AutoFlushQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoFlushQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoFlushQueue q = new AutoFlushQueue(10, 10);
+            q.OnQueueIsFull = list => PrintList(list);
+            Task.Run(() => FillQueue(q));
+            Console.ReadLine();
+        }
+
+        private static void FillQueue(AutoFlushQueue q)
+        {
+            for (int i = 0; i < 100; i++)
+                q.Enqueue(i);
+        }
+
+        private static void PrintList(List<int> list)
+        {
+            foreach (var l in list)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
